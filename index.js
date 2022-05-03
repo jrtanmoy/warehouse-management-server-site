f63718a3cd5eeff9463292e8208abc0a5dbbc51f
jrtanmoy <roy28301@gmail.com>
2022-05-03 10:41:44 +0600

    apply jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -9,26 +10,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aogl6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const inventoryCollection = client.db('groxiwarehouse').collection('inventory');
 
-        app.get('/inventory', async(req, res) =>{
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // inventories api
+        app.get('/inventory', async (req, res) => {
             const quary = {};
             const cursor = inventoryCollection.find(quary);
             const inventories = await cursor.toArray();
             res.send(inventories);
         })
 
-        app.get('/inventory/:id', async(req, res) =>{
+        app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const inventory = await inventoryCollection.findOne(query);
@@ -36,7 +61,7 @@ async function run(){
         })
 
         // post
-        app.post('/inventory', async(req, res) =>{
+        app.post('/inventory', async (req, res) => {
             const newInventory = req.body;
             const result = await inventoryCollection.insertOne(newInventory);
             res.send(result);
@@ -52,10 +77,10 @@ async function run(){
         });
 
         // update 
-        app.put('/inventory/:id', async(req, res) => {
+        app.put('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const updatedStock = req.body;
-            const filter = {_id: ObjectId(id)};
+            const filter = { _id: ObjectId(id) };
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
@@ -68,27 +93,32 @@ async function run(){
 
         // For my item 
         // get
-        app.get('/inventoryForUser', async(req, res) =>{
+        app.get('/inventoryForUser', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            console.log(email);
-            const query = {email: email};
-            const cursor = inventoryCollection.find(query);
-            const myItems = await cursor.toArray();
-            res.send(myItems);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = inventoryCollection.find(query);
+                const myItems = await cursor.toArray();
+                res.send(myItems);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
 
     }
-    finally{
+    finally {
 
     }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send('running warehouse server');
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
