0c672cffe278ca328a80b66ee519377722694800
jrtanmoy <roy28301@gmail.com>
2022-05-30 18:42:55 +0600

    some update

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
             res.send(inventories);
         })
 
+        // inventories get api for id
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
